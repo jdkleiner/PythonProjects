2e58361b04a33aa3e2d485183f13988501b0150d
Joseph Kleiner <jkleiner@vt.edu>
2022-05-09 13:40:49 -0400

    python projects

--- week6_scratch/18_speech.py
+++ week6_scratch/18_speech.py
@@ -1,5 +1,10 @@
 import pyttsx3
 
+# engine = pyttsx3.init()
+# engine.say("hello, world")
+# engine.runAndWait()
+
 engine = pyttsx3.init()
-engine.say("hello, world")
+name = input("What's your name? ")
+engine.say(f"hello,{name}")
 engine.runAndWait()
